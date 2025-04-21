c64e58fef054f24f8534f9085dfee496b82ac3f2
Cagan Sit <cagan.sit@gmail.com>
2025-04-21 23:13:49 +0300

    update wallet endpoint to include API versioning

--- src/main/java/com/cagan/walletapi/rest/controller/WalletController.java
+++ src/main/java/com/cagan/walletapi/rest/controller/WalletController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/wallets")
+@RequestMapping("/api/v1/wallets")
 @RequiredArgsConstructor
 public class WalletController {
     private final WalletMapper walletMapper = WalletMapper.INSTANCE;
